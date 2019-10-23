5dbe281e88409903abd9fd7dc687cb4fc74ab9de
Slama Mohamed Foued <medfouedslama@outlook.fr>
2019-10-23 13:58:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import UserList from "./compnents/users/usersList";
+import PostsList from "./compnents/PostsList/PostsList";
+import CommentList from "./compnents/CommentList/CommentList";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Router>
+            <Route exact path="/" component={UserList} />
+            <Route exact path="/posts/:id" component={PostsList} />
+            <Route
+              exact
+              path="/comments/:postId/:userId"
+              component={CommentList}
+            />
+          </Router>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
